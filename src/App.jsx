92e345b68c82d9bc7db9e81cf87f7d98dacc4e04
Jonghak3 <ghdwhdgkr92@gmail.com>
2025-12-27 12:31:28 +0900

    이미지파일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
-import New from './pages/New';
-import Diary from './pages/Diary';
-import Notfound from './pages/Notfound';
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
+
+import emotion1 from "./assets/emotion1.png";
+import emotion2 from "./assets/emotion2.png";
+import emotion3 from "./assets/emotion3.png";
+import emotion4 from "./assets/emotion4.png";
+import emotion5 from "./assets/emotion5.png";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 파일
@@ -12,14 +18,28 @@ function App() {
   const nav = useNavigate();
 
   const onClickButton = () => {
-    nav('/new');
+    nav("/new");
   };
   return (
     <>
       <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/new'}>New</Link>
-        <Link to={'/diary'}>Diary</Link>
+        <img src={"/emotion1.png"} />
+        <img src={"/emotion2.png"} />
+        <img src={"/emotion3.png"} />
+        <img src={"/emotion4.png"} />
+        <img src={"/emotion5.png"} />
+      </div>
+      <div>
+        <img src={emotion1} />
+        <img src={emotion2} />
+        <img src={emotion3} />
+        <img src={emotion4} />
+        <img src={emotion5} />
+      </div>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
       </div>
       <button onClick={onClickButton}>New 페이지로 이동</button>
       <Routes>
